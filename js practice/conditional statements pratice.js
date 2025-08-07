4a8e1f99750abd5feb8d15781013940d4c9e7238
Praveen N <praveen.n7050@gmail.com>
2025-08-07 06:16:59 +0530

    Add files via upload
    This repository contains my JavaScript practice programs focused on conditional statements. It includes examples using if, else if, else, and switch statements. I’m adding code regularly, covering small to intermediate problems like checking even/odd numbers, grading systems, number comparison, and more logical conditions step-by-step as I learn. This is part of my learning journey to strengthen my understanding of control flow in JavaScript.

---
+++ js practice/conditional statements pratice.js
@@ -0,0 +1,50 @@
+ let num=18;
+
+if(num%2!=0){
+    console.log("odd")  
+}else{
+    console.log("even")  
+}
+ 
+ let surya = 109;
+
+if (surya>100){
+    console.log("null")
+}else if(surya >= 90 && surya <= 100) {
+  console.log("AA");
+} else if (surya >= 80 && surya <=89) {
+  console.log("AB");
+} else if (surya >= 70 && surya <= 79) {
+  console.log("BB");
+} else if (surya >= 60 && surya <= 69) {
+  console.log("BC");
+} else if (surya >= 50 && surya <= 59) {
+  console.log("CC");
+} else if (surya >= 40 && surya <= 49) {
+  console.log("CD");
+} else if (surya >= 30 && surya <= 39) {
+  console.log("CD");
+} else if (surya <= 30 && surya >= 1) {
+  console.log("FAIL");
+}
+
+//switch statement
+let day = "monday";
+  
+switch (day) {
+  case "monday": console.log("monday is weekday");
+    break;
+  case "Tuesday":console.log("Tuesday is weekday");
+    break;
+  case "wednesday":console.log("wednesday is weekday");
+    break;
+  case "Thursday": console.log("Thursday is weekday");
+    break;
+  case "Friday":console.log("Friday is weekday");
+    break;
+  case "saturday":console.log("Saturday is weekday");
+    break;
+  case "sunday":console.log("sunday is week-end");
+    break;
+    default:console.log("its is not a day")
+}   
