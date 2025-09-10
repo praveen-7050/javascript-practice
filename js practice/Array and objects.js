adffea142276074e6b4fac33a0b42b8744745be0
Praveen N <praveen.n7050@gmail.com>
2025-09-10 06:27:16 +0530

    Update Array and objects.js

--- js practice/Array and objects.js
+++ js practice/Array and objects.js
@@ -4,3 +4,11 @@ const add=[2,5,10,20,33,55,64]
  // indexof-finding index value
 
 console.log(add.indexOf(64));
+
+// Map
+
+let sum=add.map((number)=>{
+    return number+10
+})
+
+console.log(sum)
