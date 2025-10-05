acd30d4b5390f83ae9cc8635bc398aaeab10431f
praveen-7050 <praveen.n7050@gmail.com>
2025-10-05 10:31:51 +0530

    adding classes floder

---
+++ ES6/classes/classes.js
@@ -0,0 +1,44 @@
+// console.warn("warning")
+// console.error("error")
+// console.log("log")
+// console.info("info")
+// console.clear()
+// console.count()
+// console.countReset("error")
+// console.debug("log")
+// console.table(["tabel-1", "tabel-2", "tabel-3"]);
+
+
+class Employee{
+    // class body
+    // let a=10 
+    // a=10                    // fleid
+    //  function add (){} 
+
+    // add (){}
+    // sub (){}  // we can set multiple class
+    employeeName="oscar"
+    driverName=name
+
+    forumla1(){
+        console.log(this.employeeName)
+        
+    }
+    favdriver(name){
+        console.log(this.name)
+        
+    }
+}
+// forumla1()
+let employee = new Employee()
+employee.employeeName= "lando"
+// console.log(employee.employeeName)
+employee.forumla1();
+
+let emp2 =new Employee()
+emp2.employeeName ="Norris"
+// console.log(emp2.employeeName)
+emp2.forumla1()
+
+let driver=new avdriver()
+driver.favdriver("praveen")
