7e5453e6d155b8f892a94b77b242e5799880e019
praveen-7050 <praveen.n7050@gmail.com>
2025-10-24 08:35:02 +0530

    adding api file

--- APi/API.js
+++ APi/API.js
@@ -1,10 +1,45 @@
-    async function getData() {
-      try {
-        const response = await fetch("https://api.example.com/data"); // order pizza
-        const data = await response.json(); // open pizza box
-        console.log(data); // enjoy the pizza
-      } catch (error) {
-        console.log("Something went wrong:", error); // if pizza place burned down 😅
-      }
+// async function getData() {
+//   try {
+//     const response = await fetch("https://api.example.com/data");
+//     const data = await response.json();
+//     console.log(data);
+//   } catch (error) {
+//     console.log("Something went wrong:", error);
+//   }
+// }
+// getData();
+
+//  fetch()
+//  .then()
+//  .catch()
+// syntax for fecth api
+
+fetch("https://mimic-server-api.vercel.app/todos/11")
+  .then((response) => {
+    if (response.ok) {
+      return response.json();
     }
- getData();
+  })
+  .then((data) => {
+    console.log(data);
+    // let index =4;
+    // let post =data[index] // the enpoint are not in id use this method to get the index value
+    // console.log(post)
+  })
+
+  .catch((error) => {
+    console.log(error);
+  })
+
+  fetch("https://mimic-server-api.vercel.app/movies/910784"),{
+    
+  }
+    .then((response) => {
+      if (response.ok) {
+        return response.json();
+      }
+    })
+    .then((data) => {
+      console.log(data);
+    });
+  
