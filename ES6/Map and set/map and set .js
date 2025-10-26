dc60feb56531790590fb61ac9169d1eb705f7372
praveen-7050 <praveen.n7050@gmail.com>
2025-10-26 10:15:47 +0530

    modifying Map and set

--- ES6/Map and set/map and set .js
+++ ES6/Map and set/map and set .js
@@ -22,3 +22,55 @@ let vechile=car.map((motor)=>{
     return motor.toUpperCase()
 })
 console.log(vechile)
+
+
+let arr = [1,1,2,2,3,4,4,4,4,5,6,7,8,8,8,8,9,10]
+
+let newarr = new Set(arr)
+
+console.log(arr,newarr)
+
+let newset= new Set()
+
+newset.add("one")
+newset.add("two")
+newset.add("three")
+newset.add("four")
+newset.add("five")
+newset.add(true)
+newset.add({
+    name:"praveen",
+    age:20,
+    place:"tiruppur",
+    lang:"tamil,english"
+})
+
+
+console.log(newset);
+console.log(newset.size);
+let obj = {
+  name: "praveen",
+  age: 20,
+  place: "tiruppur",
+  lang: "tamil,english",
+};
+newset.add(obj)
+console.log(newset.has(obj))
+console.log(newset.has("one"));
+console.log(newset.delete("one"))
+console.log(newset)
+
+let arr2 =Array.from(newset)
+
+console.log(arr2)
+
+// MAP
+
+const student = new Map()
+
+student.set('Name',"praveen")
+student.set("age",20)
+student.set("ROle","front End Developer")
+student.set('Name',"surya")
+console.log(JSON.parse(student))
+
