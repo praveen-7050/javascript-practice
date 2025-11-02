f94d1741732c8aeadd5ff541e6623d6de4bd30a2
praveen-7050 <praveen.n7050@gmail.com>
2025-11-02 09:22:32 +0530

    adding closure

--- closure/index.js
+++ closure/index.js
@@ -10,3 +10,33 @@ function outer(){
 }
 let result = outer()
 result()
+
+
+
+function outerfunction(){
+  let count = 0 ;
+  function innerfunction(){
+    count++ ;
+    return count ;
+  }
+  return innerfunction
+}
+
+let callingfunction = outerfunction()
+
+console.log(callingfunction())
+console.log(callingfunction())
+
+function mainfun(){
+  let  name = "praveen";
+  function subfun(){
+    let age = 28;
+    return name + age ;
+  }
+  return subfun
+}
+
+let wholeFun = mainfun()
+
+console.log(wholeFun(
+))
